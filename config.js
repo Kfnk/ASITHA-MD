979f42836458dfe760911382fce18088ac96b7cc
Kfnk <sltharumax4@gmail.com>
2024-10-26 23:02:11 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,6 +5,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
-MONGODB: process.env.MONGODB || "",//enter mongo db url හදන විදිය පල්ලෙහාබටන් එකක් ඇති
+SESSION_ID: process.env.SESSION_ID || "𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=32ITzAAa#St4bYzwSRyR2g-B0qmHchwYZdNHhEawy6xXXme8_hjM",
+MONGODB: process.env.MONGODB || "mongodb://mongo:MnyPByJfjbrbMxVoNuxqchsTsVWHpTYk@junction.proxy.rlwy.net:56728",//enter mongo db url හදන විදිය පල්ලෙහාබටන් එකක් ඇති
 };
